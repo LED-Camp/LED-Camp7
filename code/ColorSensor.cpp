3fbda607028a20b01353270c083a2587d733138f
sho-hama <shokihamana@gmail.com>
2019-08-17 12:30:07 +0900

    fua change

--- code/ColorSensor.cpp
+++ code/ColorSensor.cpp
@@ -40,7 +40,21 @@ void ColorSensor::Initialize()
     bh1745nuc_write_byte(BH1745NUC_TL_MSB, 0xFF);
 }
 
-void ColorSensor::getColor(uint16_t * r, uint16_t * g, uint16_t * b, uint16_t * c)
+void ColorSensor::getColorValue(uint16_t * r, uint16_t * g, uint16_t * b, uint16_t * c)
 {
     bh1745nuc_get_val(r, g, b, c);
+}
+
+void ColorSensor::getColor(uint16_t red,  uint16_t green, uint16_t blue, uint16_t * color)
+{
+  uint16_t max_value = 0;
+  uint16_t colors[3] = {red, green*0.5, blue};
+
+  for(int i = 0; i < 3; i++)
+  {
+    if(colors[i] > max_value){
+      max_value = colors[i];
+      *color = i + 1;
+    }
+  }
 }
